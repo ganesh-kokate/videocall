5a0d5e4452101912d86488223b95dce9efaa5139
ganesh-kokate <ganeshkokate2093@gmail.com>
2024-11-08 02:43:35 +0530

    final

--- src/main/resources/static/client.js
+++ src/main/resources/static/client.js
@@ -16,8 +16,12 @@ const iceServers = {
     ]
 };
 
+let port = window.location.port || "8181";
+
+let socketUrl = `wss://${window.location.hostname}:${port}/socket.io/`;
+
 //using io.connect we conect to server on specified address
-const socket = io.connect("/", {
+const socket = io.connect(socketUrl, {
     transports: ["websocket"]
 });
 
